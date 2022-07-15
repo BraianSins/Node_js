f04b8db2c9716372d9abc9d2f33d1445ce3487df
Braian Sins <98047036+BraianSins@users.noreply.github.com>
2022-07-14 22:45:37 -0300

    atualizações

---
+++ index.js
@@ -0,0 +1,18 @@
+const questions = [//const com as strings de pergunta
+    "O que aprendi a fazer hoje?",
+    "por que não dormi mais cedo?",
+    "o que eu podia ter melhorado pra atender?",
+    "que tipo de problema eu tive que resolver hoje?",
+    "quantas pessoas eu ajudei hoje?",
+]
+
+const ask = ( index = 0 )  => {
+    process.stdout.write(questions[index] + "\n\n\n") //stdout vai dar print no meu console (tal qual console.log, mas o PSW não troca de linha quando termina). questions = index
+}
+
+ask()
+//data = toda vez que tiver algum dado, informação, ele irá receber. data => basicamente é o que ele vai fazer com os dados
+process.stdin.on("data", data => {
+process.stdout.write( data.toString().trim() + '\n')
+process.exit()
+} )
