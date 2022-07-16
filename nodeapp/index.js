160bb833c5522dd31083420ae315ab80c4a6241c
Braian Sins <98047036+BraianSins@users.noreply.github.com>
2022-07-16 17:13:34 -0300

    commit

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -8,6 +8,7 @@ const questions = [//const com as strings de pergunta
 
 const ask = ( index = 0 )  => {
     process.stdout.write(`\n\n\n${questions[index]} > `) //stdout vai dar print no meu console (tal qual console.log, mas o PSW não troca de linha quando termina). questions = index
+
 }
 
 ask()
@@ -28,3 +29,27 @@ process.stdin.on("data", data => {
     process.exit() //finalizar processo
  }
 } )
+
+
+process.on('exit', () => {
+    console.log(`
+
+    O que você aprendeu foi: 
+    ${answers[0]}
+    
+    
+    Motivo por você não ter dormido no horário:
+    ${answers[1]}
+
+    O que você poderia ter feito parta melhorar foi:
+    ${answers[2]}
+
+    O que você poderia ter melhorado no atendimento:
+    ${answers[3]}
+
+    O tipo de problema aconteceu e você resolveu:
+    ${answers[4]}
+
+    Amanhã você volta e faz novos objetivos
+    `)
+})
---
+++ nodeapp/setTimeOut.js
@@ -0,0 +1,5 @@
+//setTimeOut vai rodar uma função depois de x Milissegundos 
+const setTimeOut = 3000;
+function finished() {
+    return console.log("Done!");
+}
